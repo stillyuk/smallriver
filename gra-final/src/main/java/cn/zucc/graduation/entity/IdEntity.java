190ff3fed06b8b55576bb738dbd66a1402310546
jiangyu <jiangyu@10.137.1.106>
2014-04-21 21:06:00 +0800

    ss

--- gra-final/src/main/java/cn/zucc/graduation/entity/IdEntity.java
+++ gra-final/src/main/java/cn/zucc/graduation/entity/IdEntity.java
@@ -1,16 +1,18 @@
 package cn.zucc.graduation.entity;
 
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @MappedSuperclass
 public class IdEntity {
 	protected Long id;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(generator = "nativeGenerator")
+	@GenericGenerator(name = "nativeGenerator", strategy = "native")
 	public Long getId() {
 		return id;
 	}
